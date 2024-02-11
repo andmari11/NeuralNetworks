50b2306bd59d1cdd4a43483ab51d4192f55b985b
andmari11 <anres.mari.piqueras@gmail.com>
2024-02-11 20:44:03 +0100

    p3

---
+++ p3.py
@@ -0,0 +1,26 @@
+#3 neuron with 4 inputs
+inputs = [1, 2, 3, 2.5]
+
+
+weights=[[0.2, 0.8, -0.5, 1],
+         [0.5, -0.91, 0.26, -0.5],
+         [-0.26, -0.27, 0.17, 0.87]]
+
+biases= [2, 3, 0.5]
+
+
+layer_output=[]
+
+#calculo de cada neurona
+for neuron_weights, neuron_bias in zip(weights, biases):
+
+    #output de cada peso y su input + bias
+    neuron_output=0
+    for neuron_input, weight in zip(inputs, neuron_weights):
+        
+        neuron_output+=neuron_input*weight
+
+    neuron_output+=neuron_bias
+    layer_output.append(neuron_output)
+
+print(layer_output)
