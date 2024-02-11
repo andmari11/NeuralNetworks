b8c3ddacfa7090c80c5a322024d2671f9eb31aa9
andmari11 <anres.mari.piqueras@gmail.com>
2024-02-11 20:26:27 +0100

    p2

--- p2.py
+++ p2.py
@@ -1,10 +1,19 @@
-inputs = [1, 2, 3]
-weights= [0.2, 0.8, -0.5]
-bias = 2
-output = 0
+#3 neuron with 4 inputs
+inputs = [1, 2, 3, 2.5]
 
-for i in range(3):   
-    output += inputs[i]*weights[i]
+weights1= [0.2, 0.8, -0.5, 1]
+weights2= [0.5, -0.91, 0.26, -0.5]
+weights3= [-0.26, -0.27, 0.17, 0.87]
 
-output+=bias
+
+bias1 = 2
+bias2 =3
+bias3 =0.5
+output=[bias1, bias2, bias3]
+
+for i in range(4):
+    output[0] += inputs[i]*weights1[i]
+    output[1] += inputs[i]*weights2[i]
+    output[2] += inputs[i]*weights3[i]
+
 print(output)
